40402b02f2016494da1d656e3f96d8848208175b
Pete Otaqui <pete.otaqui@sky.uk>
2020-11-27 13:29:53 +0000

    Initial Commit

---
+++ FlashBang/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashBang
+//
+//  Created by Pete Otaqui on 27/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashBang/FlashBangApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashBangApp.swift
+//  FlashBang
+//
+//  Created by Pete Otaqui on 27/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct FlashBangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
